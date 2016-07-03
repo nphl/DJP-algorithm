06f8ac65c5780fdc984dbb941ea22f037437588e
IsmailovR <rn.ismailov@gmail.com>
2016-07-03 15:56:54 +0300

    Add files via upload
    Изменена отрисовка графа. Добавлен ручной ввод в поле диалогового окна.

--- Controller.java
+++ Controller.java
@@ -6,13 +6,16 @@ import javafx.scene.control.*;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ResourceBundle;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Controller implements Initializable {
 
@@ -30,12 +33,18 @@ public class Controller implements Initializable {
     TextField weighttext;
 
     @FXML
-    Tab drawPage;
+    Tab drawPage = new Tab();
+    @FXML
+    Tab inputPage = new Tab();
+    @FXML
+    TabPane mainTabPane = new TabPane();
 
     @FXML
-    TextArea filefield;
+    TextArea
+    filefield;
     @FXML
-    TextArea hinputfield;
+    TextArea
+    hinputfield;
 
     @FXML
     Button drawButton;
@@ -44,26 +53,41 @@ public class Controller implements Initializable {
 
     @FXML
     Label hinputlabel;
+    @FXML
+    Label drawLbl1;
+
+    @FXML
+    Label drawLbl2;
+
+    @FXML
+    Circle
+    baseDrawCircle;
+
+    @FXML
+    private Pane drawPane;
 
     int[] inpInt = null;
     int[] readyArr = null;
     int size = 0;
-    int handInputTemp = 2;
+    int handInputTemp = 1;
     String handInputTempStr = new String();
+    private Vector<GraphStruct> abc = new Vector<>();
 
     @FXML
     public void enterPressed() {
         fileOk();
     }
+    @FXML
+    public void edgeEnterPressed() { okRBPressed(); }
+    @FXML
+    public void numOfRootsEnterPressed() { okRRPressed();};
 
     @FXML
     public void okRRPressed() {
+        if(numrootstext.getText().trim().isEmpty()) return;
         size = Integer.parseInt(numrootstext.getText());
         inpInt = new int[size * (size) + 4];
         inpInt[0] = size;
-
-        if (numribestext.getText().trim().isEmpty()) inpInt[1] = -1;
-        else inpInt[1] = Integer.parseInt(numribestext.getText());
 
         firstroottext.setVisible(true);
         secondroottext.setVisible(true);
@@ -72,10 +96,9 @@ public class Controller implements Initializable {
         hinputlabel.setVisible(true);
     }
 
-    ;
-
     @FXML
     public void okRBPressed() {
+        if(firstroottext.getText().trim().isEmpty()||secondroottext.getText().trim().isEmpty()||weighttext.getText().trim().isEmpty()) return;
         inpInt[handInputTemp] = Integer.parseInt(firstroottext.getText());
         inpInt[handInputTemp + 1] = Integer.parseInt(secondroottext.getText());
         inpInt[handInputTemp + 2] = Integer.parseInt(weighttext.getText());
@@ -83,29 +106,40 @@ public class Controller implements Initializable {
         handInputTempStr += firstroottext.getText() + " " + secondroottext.getText() + " " + weighttext.getText() + "\n";
         hinputfield.setVisible(true);
         hinputfield.setText(handInputTempStr);
-        firstroottext.setText(null);
-        secondroottext.setText(null);
-        weighttext.setText(null);
+        firstroottext.setText("");
+        secondroottext.setText("");
+        weighttext.setText("");
         drawButton.setDisable(false);
-        //resizeInput();
     }
 
     private void resizeInput() {
         int j;
         for (j = 0; j < inpInt.length; j++) if (inpInt[j] == 0) break;
         readyArr = new int[j];
-
+        System.out.println(j);
         for (int i = 0; i < j; i++) readyArr[i] = inpInt[i];
-    }
 
-    ;
+        GraphStruct temp;
+        for (int i = 0; i < (readyArr.length - 1) / 3; i++) {
+            temp = new GraphStruct(readyArr[i * 3 + 1], readyArr[i * 3 + 2], readyArr[i * 3 + 3]);
+            abc.add(temp);
+        }
+    }
 
     @FXML
     public void drawPressed() {
 
         resizeInput();
-        GraphFrame frm = new GraphFrame(readyArr);
-        frm.setVisible(true);
+        SingleSelectionModel<Tab> selectionModel = mainTabPane.getSelectionModel();
+
+        if(selectionModel.getSelectedIndex() == 0){
+            selectionModel.select(1);
+        } else {
+            selectionModel.select(0);
+        }
+        drawPage.setDisable(false);
+        drawGraph();
+        //drawPage.
 
     }
 
@@ -131,18 +165,62 @@ public class Controller implements Initializable {
                     int ibuff = Integer.parseInt(stoken.nextToken());
                     inpInt[i] = ibuff;
                     buff2 += Integer.toString(ibuff) + " ";
-                    if (i % 3 == 1) buff2 += "\n";
+                    if (i % 3 == 0) buff2 += "\n";
                     i++;
                 }
             }
         } catch (IOException ex) {
 
         }
-
+        size = inpInt[0];
         filefield.setText(buff2);
         filefield.setVisible(true);
-        drawPage.setDisable(false);
+        //drawPage.setDisable(false);
         drawButton.setDisable(false);
+    }
+
+    private void drawGraph(){
+
+        List CircList = new ArrayList<>(); //точки
+        List LblList = new ArrayList<>(); //лейблы
+        List LineList = new ArrayList<>(); // линии
+        Label V1, V2, V3;
+        Line edge;
+        double x1,x2,y1,y2;
+        int shiftx = 0, shifty=0;
+        for (int i = 0; i < abc.size(); i++) {
+            x1 = baseDrawCircle.getRadius() * Math.cos(2 * Math.PI / size*abc.get(i).first) + baseDrawCircle.getCenterX();
+            y1 = baseDrawCircle.getRadius() * Math.sin(2 * Math.PI / size * abc.get(i).first) + baseDrawCircle.getCenterY();
+            x2 = baseDrawCircle.getRadius() * Math.cos(2 * Math.PI / size * abc.get(i).second) + baseDrawCircle.getCenterX();
+            y2 = baseDrawCircle.getRadius() * Math.sin(2 * Math.PI / size * abc.get(i).second) + baseDrawCircle.getCenterY();
+          //  CircList.add(new Circle(x1, y1, 7, Color.GRAY));
+          //  CircList.add(new Circle(x2, y2, 7, Color.GRAY));
+          //  LblList.add(new Label(" " + i));
+            edge = new Line(x1, y1, x2, y2);
+            edge.setStrokeWidth(1);
+            edge.setStroke(Color.WHITE);
+            LineList.add(edge);
+            drawPane.getChildren().addAll(edge, new Circle(x1, y1, 9, Color.CORNFLOWERBLUE), new Circle(x2, y2, 9, Color.CORNFLOWERBLUE));
+            x1 += -4;
+            y1 += -9;
+            x2 += -4;
+            y2 += -9; //рисование имен вершин
+            V1 = new Label("" + abc.get(i).first);
+            V1.setLayoutX(x1);
+            V1.setLayoutY(y1);
+            V2 = new Label("" + abc.get(i).second);
+            V2.setLayoutX(x2);
+            V2.setLayoutY(y2);
+            V3 = new Label("" + abc.get(i).weight);
+            V3.setLayoutX((x1 + x2)/2 + shiftx);
+            V3.setLayoutY((y1 + y2)/2 + shifty);
+            V1.setStyle("-fx-font-weight: bold; -fx-font-size: 14px; -fx-text-fill: #660000; -fx-font-family: \"Impact\";");
+            V2.setStyle("-fx-font-weight: bold; -fx-font-size: 14px; -fx-text-fill: #660000; -fx-font-family: \"Impact\";");
+            V3.setStyle("-fx-font-weight: bold; -fx-font-size: 14px; -fx-text-fill: #FFFFFF; -fx-font-family: \"Impact\";");
+            drawPane.getChildren().addAll(V1, V2, V3);
+        }
+
+
     }
 
     @Override
@@ -157,6 +235,7 @@ public class Controller implements Initializable {
         secondroottext.setVisible(false);
         weighttext.setVisible(false);
         hinputlabel.setVisible(false);
+       // mainTabPane.getTabs().addAll(inputPage, drawPage);
     }
 
 
---
+++ GraphStruct.java
@@ -0,0 +1,18 @@
+package sample;
+
+/**
+ * Created by rossh on 03.07.2016.
+ */
+public class GraphStruct {
+
+    public int first;
+    public int second;
+    public double weight;
+
+        public GraphStruct(int first, int second, double weight) {
+            this.first = first;
+            this.second = second;
+            this.weight = weight;
+
+    }
+}
