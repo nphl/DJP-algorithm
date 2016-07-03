d1e1890297e969f943db0a66844f957a1be4d8bd
Konstantin <nphl@users.noreply.github.com>
2016-07-03 08:42:26 +0300

    Revert "Redacted indents"
    This reverts commit f8af0af179ea8160ed41524c5d5e5bb928063766.

--- Algorithm.java
+++ Algorithm.java
@@ -7,49 +7,43 @@ import java.util.Vector;
  */
 public class Algorithm {
 
-    private int min(int a, int b) {
-        if (a < b) return a;
+    private int min(int a, int b){
+        if(a<b) return a;
         return b;
     }
-
-    private void fill(int[] toFill, int value) {
-        for (int i = 0; i < toFill.length; i++) toFill[i] = value;
+    private void fill(int[] toFill, int value){
+        for(int i=0; i<toFill.length;i++) toFill[i] = value;
     }
-
-    private void fill2(int[][] tof, int size, int value) {
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) tof[i][j] = value;
+    private void fill2(int [][]tof,int size, int value){
+        for(int i=0; i<size;i++){
+            for(int j=0;j<size;j++) tof[i][j] = value;
         }
-    }
+    };
 
-    ;
+public int INF = Integer.MAX_VALUE / 2; // "Бесконечность"
+public int [][] reDraw;
+    public int mstPrim(int [][] graph, int vNum) {
 
-    public int INF = Integer.MAX_VALUE / 2; // "Бесконечность"
-    public int[][] reDraw;
-
-    public int mstPrim(int[][] graph, int vNum) {
-
-        boolean[] used = new boolean[vNum]; // массив пометок
+        boolean[] used = new boolean [vNum]; // массив пометок
         int[][] ddist = new int[vNum][3];
-        for (int i = 0; i < vNum; i++) ddist[i][0] = 0;
-        int[] dist = new int[vNum]; // массив расстояния. dist[v] = вес_ребра(MST, v)
+        for(int i=0;i<vNum;i++)ddist[i][0] = 0;
+        int[] dist = new int [vNum]; // массив расстояния. dist[v] = вес_ребра(MST, v)
         reDraw = new int[vNum][vNum];
-        fill2(reDraw, vNum, 0);
+        fill2(reDraw,vNum,0);
         fill(dist, INF); // устанаавливаем расстояние до всех вершин INF
         dist[0] = 0; // для начальной вершины положим 0
         ddist[0][0] = 0;
-        for (; ; ) {
+        for (;;) {
             int v = -1;
             for (int nv = 0; nv < vNum; nv++) // перебираем вершины
-                if (!used[nv] && dist[nv] < INF && (v == -1 || dist[v] > dist[nv]))
-                    v = nv;// выбираем самую близкую непомеченную вершину
+                if (!used[nv] && dist[nv] < INF && (v == -1 || dist[v] > dist[nv]))  v = nv;// выбираем самую близкую непомеченную вершину
 
-            if (v == -1) break; // ближайшая вершина не найдена
-            used[v] = true; // помечаем ее
+                if (v == -1) break; // ближайшая вершина не найдена
+                    used[v] = true; // помечаем ее
             for (int nv = 0; nv < vNum; nv++) {
                 if (!used[nv] && graph[v][nv] < INF) { // для всех непомеченных смежных
                     dist[nv] = min(dist[nv], graph[v][nv]); // улучшаем оценку расстояния (релаксация)
-                    if (graph[v][nv] == dist[nv]) {
+                    if(graph[v][nv] == dist[nv]){
                         ddist[nv][0] = dist[nv];
                         ddist[nv][1] = v;
                         ddist[nv][2] = nv;
@@ -57,14 +51,14 @@ public class Algorithm {
                 }
             }
 
-        }
-        for (int i = 1; i < vNum; i++) {
-            if (ddist[i][0] == 0) break;
+                    }
+        for(int i=1; i<vNum;i++){
+            if(ddist[i][0]==0) break;
             reDraw[ddist[i][1]][ddist[i][2]] = ddist[i][0];
         }
         int ret = 0; // вес MST
-        for (int v = 0; v < vNum; v++)
-            ret += dist[v];
+         for (int v = 0; v < vNum; v++)
+        ret += dist[v];
         return ret;
     }
 }
--- Controller.java
+++ Controller.java
@@ -5,7 +5,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Label;
 
 import java.io.DataInputStream;
 import java.io.FileInputStream;
@@ -14,109 +13,36 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.StringTokenizer;
 
-public class Controller implements Initializable {
+public class Controller implements Initializable{
 
-    @FXML
-    TextField fileinptext;
-    @FXML
-    TextField numrootstext;
-    @FXML
-    TextField numribestext;
-    @FXML
-    TextField firstroottext;
-    @FXML
-    TextField secondroottext;
-    @FXML
-    TextField weighttext;
-
-    @FXML
-    Tab drawPage;
-
-    @FXML
-    TextArea filefield;
-    @FXML
-    TextArea hinputfield;
-
-    @FXML
-    Button drawButton;
-    @FXML
-    Button okribebutton;
-
-    @FXML
-    Label hinputlabel;
-
-    int[] inpInt = null;
-    int[] readyArr = null;
-    int size = 0;
-    int handInputTemp = 2;
-    String handInputTempStr = new String();
-
-    @FXML
-    public void enterPressed() {
+    @FXML TextField fileinptext;
+    @FXML Tab drawPage;
+    @FXML TextArea filefield;
+    @FXML Button drawButton;
+    int[] inpInt=null;
+    int[] readyArr=null;
+    int size=0;
+    @FXML public void enterPressed(){
         fileOk();
     }
-
-    @FXML
-    public void okRRPressed() {
-        size = Integer.parseInt(numrootstext.getText());
-        inpInt = new int[size * (size) + 4];
-        inpInt[0] = size;
-
-        if (numribestext.getText().trim().isEmpty()) inpInt[1] = -1;
-        else inpInt[1] = Integer.parseInt(numribestext.getText());
-
-        firstroottext.setVisible(true);
-        secondroottext.setVisible(true);
-        weighttext.setVisible(true);
-        okribebutton.setVisible(true);
-        hinputlabel.setVisible(true);
-    }
-
-    ;
-
-    @FXML
-    public void okRBPressed() {
-        inpInt[handInputTemp] = Integer.parseInt(firstroottext.getText());
-        inpInt[handInputTemp + 1] = Integer.parseInt(secondroottext.getText());
-        inpInt[handInputTemp + 2] = Integer.parseInt(weighttext.getText());
-        handInputTemp += 3;
-        handInputTempStr += firstroottext.getText() + " " + secondroottext.getText() + " " + weighttext.getText() + "\n";
-        hinputfield.setVisible(true);
-        hinputfield.setText(handInputTempStr);
-        firstroottext.setText(null);
-        secondroottext.setText(null);
-        weighttext.setText(null);
-        drawButton.setDisable(false);
-        //resizeInput();
-    }
-
-    private void resizeInput() {
+    @FXML public void drawPressed(){
         int j;
-        for (j = 0; j < inpInt.length; j++) if (inpInt[j] == 0) break;
+        for(j=0; j<inpInt.length;j++) if(inpInt[j]==0) break;
         readyArr = new int[j];
-
-        for (int i = 0; i < j; i++) readyArr[i] = inpInt[i];
-    }
-
-    ;
-
-    @FXML
-    public void drawPressed() {
-
-        resizeInput();
+        for(int i=0; i<j; i++) readyArr[i] = inpInt[i];
         GraphFrame frm = new GraphFrame(readyArr);
         frm.setVisible(true);
 
     }
 
-    @FXML
-    public void fileOk() {
-        int i = 0;
-        String buff, buff2 = new String();
-        DataInputStream input = null;
-        try (DataInputStream streambuff = new DataInputStream(new FileInputStream(fileinptext.getText()))) {
+    @FXML public void fileOk(){
+        int  i=0;
+        String buff, buff2= new String();
+        DataInputStream input=null;
+        try(DataInputStream streambuff = new DataInputStream(new FileInputStream(fileinptext.getText()))){
             size = streambuff.available();
-        } catch (IOException ex2) {
+        }
+        catch (IOException ex2){
             filefield.setText("Wrong path");
             filefield.setVisible(true);
             return;
@@ -125,17 +51,18 @@ public class Controller implements Initializable {
         try {
             input = new DataInputStream(new FileInputStream(fileinptext.getText()));
 
-            while ((buff = input.readLine()) != null) {
+            while((buff = input.readLine())!=null){
                 StringTokenizer stoken = new StringTokenizer(buff);
-                while (stoken.hasMoreTokens()) {
+                while(stoken.hasMoreTokens()){
                     int ibuff = Integer.parseInt(stoken.nextToken());
                     inpInt[i] = ibuff;
-                    buff2 += Integer.toString(ibuff) + " ";
-                    if (i % 3 == 1) buff2 += "\n";
+                    buff2 += Integer.toString(ibuff)+" ";
+                    if(i%3==1) buff2+="\n";
                     i++;
                 }
             }
-        } catch (IOException ex) {
+        }
+        catch(IOException ex){
 
         }
 
@@ -146,17 +73,11 @@ public class Controller implements Initializable {
     }
 
     @Override
-    public void initialize(URL location, ResourceBundle res) {
+    public void initialize(URL location, ResourceBundle res){
 
         drawPage.setDisable(true);
         filefield.setVisible(false);
         drawButton.setDisable(true);
-        hinputfield.setVisible(false);
-        okribebutton.setVisible(false);
-        firstroottext.setVisible(false);
-        secondroottext.setVisible(false);
-        weighttext.setVisible(false);
-        hinputlabel.setVisible(false);
     }
 
 
